16ad6c9db198e95b4e60bff74b4904c5a2827bf9
chadayas <cgassoya@gmail.com>
2026-01-12 16:31:14 -0500

    debugging fns

--- server.cpp
+++ server.cpp
@@ -7,6 +7,7 @@
 #include<cctype>
 #include<iomanip>
 #include<iterator>
+#include<sstream>
 
 enum class State {
 	MULTIPART_HEADER,
@@ -50,25 +51,67 @@ void  debug_buffer(char buffer[]){
 	std::cout << std::endl;	
 
 }
+void print_stash(const std::string& s){
+	for (std::size_t i = 0; i < s.size() ; ++i){
+		unsigned char c = s[i];	
+		if (c == '\r')
+			std::cout << "\\r";
+		else if (c == '\n')
+			std::cout << "\\n";
+		else if(c >= 32 && c < 127)
+			std::cout << c;
+		else 
+			std::cout << std::hex <<"[" <<static_cast<int>(c) << "]";
+	}	
+	std::cout << std::endl;
+	}
+
+// fn to return the stash as a string [DEBUG]
+std::string hold_stash(const std::string& s){
+	std::string ret;	
+	for (std::size_t i = 0; i < s.size() ; ++i){
+		unsigned char c = s[i];	
+		if (c == '\r')
+			 ret += "\\r";
+		else if (c == '\n')
+			ret += "\\n";
+		else if(c >= 32 && c < 127)
+			ret.push_back(c);
+		else {
+			std::stringstream ss;
+			ss << std::hex <<"[" <<static_cast<int>(c) << "]";
+			ret += ss.str();	
+		}
+	}
+	return ret;	
+}
+
+
+
+
+
 void  debug_buffer2(char buffer[]){
-	//std::string ct = "Content-Type: image/jpeg";	
-	//std::string str_buf(buffer);
-	//auto idx = str_buf.find(ct);
-	const char* ct = "Content-Type: image/";	
-	char* str_ptr = std::strstr(buffer, ct);
-	if (str_ptr == NULL){
+	std::string ct = "Content-Type: image/";	
+	std::string del = "\r\n\r\n";	
+	std::string str_buf(buffer);
+	auto idx = str_buf.find(ct);
+	auto idx_i = str_buf.find(del, idx);	
+	//const char* ct = "Content-Type: image/";	
+	//char* str_ptr = std::strstr(buffer, ct);
+	if (idx == std::string::npos){
 		std::cout << "Content-Type DNE" << std::endl;
 		return;
 	}
-	std::size_t idx = str_ptr - buffer;
-	std::size_t sz = 	
-	for (std::size_t i = 0; i < 50 ; ++i){
-		if (buffer[idx + i ] == '\r')
+	for (std::size_t i = 0; i < str_buf.size() ; ++i){
+		unsigned char c = str_buf[idx_i + i];	
+		if (c == '\r')
 			std::cout << "\\r";
-		else if (buffer[idx + i] == '\n')
+		else if (c == '\n')
 			std::cout << "\\n";
-		else
-			std::cout << buffer[idx + i];	
+		else if(c >= 32 && c < 127)
+			std::cout << c;
+		else 
+			std::cout << std::hex <<"[" <<static_cast<int>(c) << "]";
 	}	
 	std::cout << std::endl;
 	}
@@ -124,25 +167,30 @@ int main()
 	std::cout <<  std::endl <<"---This is the next buffer ----"
 		<<std::endl << buf  << std::endl;	
 
-	std::ofstream f1("pic1.jpeg", std::ios::out | std::ios::binary);
 	
-	debug_buffer2(buffer);
-	std::cout << std::endl;
 	int content_length = parse_cl(buffer);
 	int total_recieved = pic_bytes;	
+	int file_count = 0;
+	std::ofstream current_file;
 	std::string wbkit_bound = find_boundary(buffer);
 	State state = State::MULTIPART_HEADER;
 	while (state != State::DONE){
 		int bytes = recv(client_socket, buffer, sizeof(buffer), 0);
 		stash.append(buffer, bytes);	
-		total_recieved += bytes;	
 		bool run = true;
 	    while (run){
 		    switch (state){
 			case State::MULTIPART_HEADER:{
-		 		std::string c_type = "Content-Type: image/jpeg";
+		 		std::string c_type = "Content-Type: image/";
+				std::string ctrl_char = "\r\n\r\n";	
 				auto idx = stash.find(c_type);
-				stash.erase(0, idx + c_type.size()+ 4);		
+				auto pos = stash.find(ctrl_char, idx);		
+				stash.erase(0, pos + 4);		
+				file_count++;	
+				std::cout <<"[State::M_H] We are on file " << file_count << "\n";	
+				current_file.open("pic" + std::to_string(file_count) 
+				+ ".jpeg", std::ios::out | std::ios::binary);
+				
 				state = State::FILE_DATA;
 				run = true;	
 				break;	
@@ -150,15 +198,25 @@ int main()
 		    	case State::FILE_DATA:{
 				auto pos = stash.find(wbkit_bound);
 				if (pos != std::string::npos){
-				   f1.write(stash.data(), pos);
-		           	  stash.erase(0, pos + wbkit_bound.size());
-		       	     	  state = State::DONE;    
-			   	 run = true;	
+				   std::cout << " [State::FILE_DATA] We are on file " 
+					   << file_count << "\n";	
+				
+				   current_file.write(stash.data(), pos);
+		           	   stash.erase(0, pos + wbkit_bound.size());
+				  	 
+				   run = true;	
+			           if (stash.substr(0, 2) == "--"){
+					   current_file.close(); 
+					   state = State::DONE;
+				   } else{ 
+					   current_file.close();
+					   state = State::MULTIPART_HEADER; 
+				   }
 				} else {
 				  std::size_t tail = wbkit_bound.size();
 				  if (stash.size() > tail){
 					  std::size_t write = stash.size() - tail;
-					  f1.write(stash.data(), write);
+					  current_file.write(stash.data(), write);
 					  stash.erase(0, write); 
 				  }
 				  run = false;
@@ -169,14 +227,12 @@ int main()
 			  case State::DONE:
 		   		break; 
 		    }
-	    if (state == State::DONE)
-		    run = false;
+	    	if (state == State::DONE)
+			break;
+	    	}
 	    }
 
-	}
-
-	f1.close();
-	std::cout <<"INSIDE THE STASH" <<  std::endl << stash << std::endl;
+	
 	close(server_socket);
 
 	return 0;
