fc06190d5f80bbe768f9903fb758cd0fced80ef7
chadayas <cgassoya@gmail.com>
2026-01-17 17:54:23 -0500

    fixes

--- mdns.cpp
+++ mdns.cpp
@@ -2,7 +2,7 @@
 #include<arpa/inet.h>
 #include<string>
 #include<iostream>
-
+#include<sys/time.h>
 
 int main(){
 	
@@ -10,6 +10,7 @@ int main(){
 	sockaddr_in bind_addr;	
 	in_addr mul_ips;
 	ip_mreq group;
+	timeval tv;	
 	
 	int socket_test = socket(AF_INET, SOCK_DGRAM, 0);
 	if( socket_test < 0){
@@ -18,28 +19,55 @@ int main(){
 		std::cout << "[WORKING:SERVER] SOCKET IS GOOD....." << std::endl;
 	}
 	group_sock.sin_family = AF_INET;
-	group_sock.sin_port = htons(5555);
-	group_sock.sin_addr.s_addr = inet_addr("225.1.1.1");
+	group_sock.sin_port = htons(4321);
+	group_sock.sin_addr.s_addr = ("225.1.1.1");
 	
 	bind_addr.sin_family = AF_INET;
-	bind_addr.sin_port = htons(5555);
-	bind_addr.sin_addr.s_addr = inet_addr("225.1.1.1");
+	bind_addr.sin_port = htons(4321);
+	bind_addr.sin_addr.s_addr = INADDR_ANY;
 
 	group.imr_multiaddr.s_addr =  inet_addr("225.1.1.1");
 	group.imr_interface.s_addr = INADDR_ANY;
 	
-	char buffer[1024] = "Welcome";
+	tv.tv_sec = 1;
+	tv.tv_usec = 0;
+	char buffer[1024] = {0};
 	mul_ips.s_addr = INADDR_ANY; 
+	
 	int sock_op = setsockopt(socket_test, IPPROTO_IP, IP_MULTICAST_IF, 
 	(struct in_addr*)&mul_ips, sizeof(mul_ips));
-	
-	setsockopt(socket_test, IPPROTO_IP, IP_ADD_MEMBERSHIP, &group, sizeof(group));
-	bind(socket_test, (struct sockaddr*)&group, sizeof(group));
+	if (sock_op <0){
+		std::cout << "[ERROR:SERVER] Socket OPTION 1 BAD" << std::endl;
+	} else{
+		std::cout << "[WORKING:SERVER] SOCKET OPTION 1 GOOD....." << std::endl;
+	}
 
+		
+	int sock_op2 = setsockopt(socket_test, IPPROTO_IP, IP_ADD_MEMBERSHIP, 
+			(char *)&group, sizeof(group));
+	if (sock_op2 <0){
+		std::cout << "[ERROR:SERVER] Socket OPTION 2 BAD" << std::endl;
+	} else{
+		std::cout << "[WORKING:SERVER] SOCKET OPTION 2 GOOD....." << std::endl;
+	}
+	
+	int bindd = bind(socket_test, (struct sockaddr*)&bind_addr, sizeof(bind_addr));
+	if (bindd < 0){
+		std::cout << "[ERROR:SERVER] BIND BAD" << std::endl;
+	} else{
+		std::cout << "[WORKING:SERVER] BIND IS GOOD....." << std::endl;
+	}
+	
+	setsockopt(socket_test, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+	
 	socklen_t group_len = sizeof(group_sock);	
 	int bytes = recvfrom(socket_test, buffer, sizeof(buffer), 0,
 		(struct sockaddr*)&group_sock, &group_len);
-		
+	if (bytes < 0)
+		std::cout << "[ERROR:SERVER] NO DATA RECIEVED" << std::endl;
+	else
+		std::cout << "[WORKING:SERVER] GOOD RECIEVE" << std::endl;
+
 	int send_fn = sendto(socket_test, buffer, sizeof(buffer),
 		0, (struct sockaddr*)&group_sock, sizeof(group_sock));
 	if (send_fn < 0){
@@ -48,12 +76,11 @@ int main(){
 		std::cout << "[WORKING:SERVER] MESSAGE SENT....." << std::endl;
 	}
 	
-	if (bytes > 0){
+	/*if (bytes > 0){
 		std::cout << "Yes this is working" << std::endl;
 		std::cout << buffer << std::endl;
-		running = false;
 		}		
 	else
 		std::cout << "Bytes: " << bytes << std::endl;
-	return 0;
+	*/return 0;
 }
--- tests/c_mdns.cpp
+++ tests/c_mdns.cpp
@@ -8,6 +8,9 @@
 int main(){
 	sockaddr_in group_sock;	
 	struct ip_mreq group;	
+	in_addr multicast_if;
+	sockaddr_in bind_addr;
+
 	char message[1024] = "Where is the photo transfer"; 
 	char buffer[1024] = {0}; 
 	int c_sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -17,29 +20,50 @@ int main(){
 		std::cout << "[WORKING:CLIENT] SOCKET IS GOOD..." << std::endl;
 	
 	group_sock.sin_family = AF_INET;
-	group_sock.sin_port = htons(5555);
+	group_sock.sin_port = htons(4321);
 	group_sock.sin_addr.s_addr = inet_addr("225.1.1.1");
+	
+	bind_addr.sin_family = AF_INET;
+	bind_addr.sin_port = htons(4321);
+	bind_addr.sin_addr.s_addr = INADDR_ANY; 
+
+	multicast_if.s_addr = INADDR_ANY;
 
 	int reuse = 1;	
-	int sock_op = setsockopt(c_sock, SOL_SOCKET, SO_REUSEADDR, 
+	/*int sock_op = setsockopt(c_sock, SOL_SOCKET, SO_REUSEADDR, 
 			(char *) &reuse, sizeof(reuse));
-	
-	int b_fn = bind(c_sock, (struct sockaddr*)&group_sock, sizeof(group_sock));	
+	if (sock_op < 0)
+		std::cout << "[ERROR:CLIENT] SOCK OPTIONS 1 BAD" << std::endl;
+	else
+		std::cout << "[WORKING:CLIENT] SOCK OPTIONS 1 GOOD" << std::endl;
+	*/	
+	int b_fn = bind(c_sock, (struct sockaddr*)&bind_addr, sizeof(bind_addr));	
 	
 	group.imr_multiaddr.s_addr = inet_addr("225.1.1.1");
 	group.imr_interface.s_addr = INADDR_ANY;
 
+	int sock_opp = setsockopt(c_sock, IPPROTO_IP, IP_MULTICAST_IF,
+			(char*)&multicast_if, sizeof(multicast_if));	
+	
 	int sock_op2 = setsockopt(c_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
 		(char*)&group, sizeof(group));
+	if (sock_op2 < 0)
+		std::cout << "[ERROR:CLIENT] SOCK OPTIONS 2 BAD" << std::endl;
+	else
+		std::cout << "[WORKING:CLIENT] SOCK OPTIONS 2 GOOD" << std::endl;
+	
+
 	int sent = sendto(c_sock, message, sizeof(message),0,
 			(struct sockaddr*)&group_sock, sizeof(group_sock));	
 		
 	if (sent < 0)
 		std::cout << "[ERROR] BAD SEND";
 	else{
-		std::cout << "[WORKING:CLIENT] SEND GOOD....";
-		std::cout << "MESSAGE FROM SERVER: " << buffer << std::endl;
-		}
+		std::cout << "[WORKING:CLIENT] SEND GOOD" << std::endl;
+		std::cout << "[WORKIN:CLIENT] Sent " << sent << " Bytes" << std::endl;	
+	}	
 
 	int recieve = recv(c_sock, buffer, sizeof(buffer), 0);
+	std::cout << buffer << std::endl;
+
 }
